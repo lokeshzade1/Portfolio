f9210c46886282bdc2be2588a9e6282e78e00a40
lokeshzade1 <zadelokesh1@gmail.com>
2023-08-04 15:05:44 +0530

    from mine

--- src/componant/Skills.js
+++ src/componant/Skills.js
@@ -15,15 +15,105 @@ const Skill = () => {
         <h1 className='text-orange-400 text-4xl font-bold'>Skills</h1>
         <div className=' w-full rounded flex  justify-center items-center mt-5'>
           <div className='flex flex-wrap gap-4 justify-evenly'>
-            <FaNodeJs className='text-green-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
-            <BiLogoJavascript className='text-yellow-400 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded  hover:bg-white ' />
-            <SiTailwindcss className='text-blue-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded  hover:bg-white' />
-            <FaGithub className='text-black bg-white h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
-            <SiMongodb className='text-green-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
-            <SiMongoose className='text-red-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
-            <TbBrandReactNative className='text-blue-400  h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
-            <TfiHtml5 className='h-40 w-40 text-yellow-400  border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
-            <SiExpress className='h-40 w-40 text-yellow-400  border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <FaNodeJs className='text-green-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>Node</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <BiLogoJavascript className='text-yellow-400 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded  hover:bg-white ' />
+              <h1 className=' text-orange-400 font-bold'>JavaScript</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-26 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <SiTailwindcss className='text-blue-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded  hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>Tailwindcss</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-28 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <FaGithub className='text-black bg-white h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>GitHub</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <SiMongodb className='text-green-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>Mongodb</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <SiMongoose className='text-red-700 h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>Mongoose</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <TbBrandReactNative className='text-blue-400  h-40 w-40 border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>React</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <TfiHtml5 className='h-40 w-40 text-yellow-400  border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>HTML</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
+            <div className='flex justify-center items-center flex-col gap-2'>
+              <SiExpress className='h-40 w-40 text-yellow-400  border-[0.5px] border-orange-400 p-2 rounded hover:bg-white' />
+              <h1 className=' text-orange-400 font-bold'>Express</h1>
+              <div className='flex justify-center items-center text-white font-bold gap-2'>
+                <h1>0</h1>
+                <div className='w-28 h-2 bg-orange-200  rounded  '>
+                  <div className='w-24 h-2 bg-orange-500  rounded hover:bg-green-600 '></div>
+                </div>
+                <h1>100</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
